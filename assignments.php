18edd1d64bc9a3e3b95ba2de18562c3c4d67380c
Adam Quinton <quintonish@gmail.com>
2016-09-23 14:27:04 -0600

    Add excersises

---
+++ assignments.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Assignment List | PHP Class</title>
+        <meta name="description" content="PHP Class Assignments">
+        <meta name="keywords" content="php, programming, byui">
+        <link href="/php-class/styles/fixie.css" type="text/css" rel="stylesheet" media="screen" />
+        <meta name="author" content="Adam Quinton">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+      <div id="page">
+        <h1 id="logo" title="howtoterminal.com">Exercises</h1>
+        <main class="column1">
+          <article>
+              <dl>
+                <dt>Assignments</dt>
+                <dd>Exercise 2-1 (Completed)
+                <dd>Exercise 3-1 (Not Completed)</dd>
+                <dd>Exercise 16 (Not Completed)</dd>
+                <dd>Exercise 4-1 (Not Completed)</dd>
+                <dd>Exercise 5-1 (Not Completed)</dd>
+                <dd>Exercise 7-2 (Not Completed)</dd>
+                <dd>Exercise 8-2 (Not Completed)</dd>
+                <dd>Exercise 15-1 (Not Completed)</dd>
+                <dd>Exercise 12-2 (Not Completed)</dd>
+                <dd>Exercise 13-1 (Not Completed)</dd>
+              </dl>
+              <dl>
+                <dt>Enhancements</dt>
+                <dd>Enhancement 2-1 (Not Completed)</dd>
+                <dd>Enhancement 4-1 (Not Completed)</dd>
+                <dd>Enhancement 5-1 (Not Completed)</dd>
+                <dd>Enhancement 7-2 (Not Completed)</dd>
+                <dd>Enhancement 12-2 (Not Completed)</dd>
+                <dd>Enhancement 13-1 (Not Completed)</dd>
+              </dl>
+
+              <p><a href="javascript:history.back()">Back</a></p>
+            </article>
+            <footer>
+              <span id="timestampy"><?php echo 'Last updated: <time datetime="'. date('c') . '">' . date('F j, Y', getlastmod()) . '</time>'; ?></span>
+            </footer>
+            </main>
+        </div>
+    </body>
+</html>
