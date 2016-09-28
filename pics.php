ba9ceda7958ef98a8a89ec08a41de981414e5e2d
Adam Quinton <quintonish@gmail.com>
2016-09-28 12:46:15 -0600

    Styles fast

--- pics.php
+++ pics.php
@@ -4,7 +4,6 @@
         <title>Assignment List | PHP Class</title>
         <meta name="description" content="PHP Class Assignments">
         <meta name="keywords" content="php, programming, byui">
-        <link href="/php-class/styles/fixie.css" type="text/css" rel="stylesheet" media="screen" />
         <meta name="author" content="Adam Quinton">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
@@ -27,5 +26,21 @@
             </footer>
             </main>
         </div>
+        <noscript id="deferred-styles">
+          <link rel="stylesheet" type="text/css" href="/php-class/styles/fixie.css"/>
+        </noscript>
+        <script>
+          var loadDeferredStyles = function() {
+            var addStylesNode = document.getElementById("deferred-styles");
+            var replacement = document.createElement("div");
+            replacement.innerHTML = addStylesNode.textContent;
+            document.body.appendChild(replacement)
+            addStylesNode.parentElement.removeChild(addStylesNode);
+          };
+          var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+              webkitRequestAnimationFrame || msRequestAnimationFrame;
+          if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+          else window.addEventListener('load', loadDeferredStyles);
+        </script>
     </body>
 </html>
