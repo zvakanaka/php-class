9814dac1af0bda43240394daa7423d38a0b1d2b6
Adam Quinton <quintonish@gmail.com>
2016-10-05 21:03:37 -0600

    Light box

--- pics.php
+++ pics.php
@@ -1,4 +1,38 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/php-class/part/head.php'; ?>
+<style>
+.black_overlay{
+  display: none;
+  position: fixed;
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  z-index:1001;
+  -moz-opacity: 0.8;
+  opacity:.80;
+  filter: alpha(opacity=80);
+}
+.white_content {
+  display: none;
+  position: fixed;
+  top: 2%;
+  left: 1%;
+  width: 95%;
+  height: 95%;
+  padding: 0px;
+  z-index:1002;
+  overflow: hidden;
+}
+#lightbox-picture {
+  height: 100%;
+}
+#lightbox-sidebar {
+  float: right;
+  color: white;
+  z-index:1003;
+}
+</style>
 <ul class="sub-nav-ul">
 <?php
 if ($handle = opendir('../photo')) {
@@ -18,15 +52,35 @@ if ($handle = opendir('../photo')) {
 }
 ?>
 </ul>
+<br><br>
+		<div id="light" class="white_content">
+      <img id="lightbox-picture"/>
+      </div>
+		<div id="fade" class="black_overlay" onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">
+
+    <div id="lightbox-sidebar">
+      <a href="javascript:void(0)" onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</a></div>
+    </div>
 
 <article>
 <?php
   $album = $_GET['album'];
   $dirname = "../photo";
-  $images = glob($dirname."/$album"."/.thumb/*.???*");
-  if (count($images) ) {
-    foreach($images as $image) {
-      echo '<a class="thumb-link" href="'.$dirname."/$album".substr($image, strrpos($image, "/")).'"><img class="thumb" src="'.$image.'" /></a>';
+  //web-sized images
+  $webs = glob($dirname."/$album"."/.web/*.???*");
+  if (!count($webs))
+    echo "<form method="
+    .'"post" action="create_webs.php"><p><input name="album" type="submit" value="'
+    .$_GET['album'].'">Create Webs</input></p>';
+
+  $thumbs = glob($dirname."/$album"."/.thumb/*.???*");
+  if (count($thumbs) ) {
+    foreach($thumbs as $image) {
+      // echo '<a class="thumb-link" href="'.$dirname."/$album/.web".substr($image, strrpos($image, "/"))
+      //   .'"><img class="thumb" src="'.$image.'" /></a>';
+        echo '<a class="thumb-link" href="javascript:void(0)" onclick="getAndShow(\''
+                .$dirname."/$album/.web".substr($image, strrpos($image, "/"))
+                .'\')"><img class="thumb" src="'.$image.'" /></a>';
     }
   } else {
     echo "<form method=".
@@ -37,4 +91,5 @@ if ($handle = opendir('../photo')) {
   echo '<div class="bottom-spacer"></div>';
 ?>
 </article>
+<script src="lightbox.js"></script>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/php-class/part/foot.php'; ?>
