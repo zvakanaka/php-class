70198ce835252c5587b4b420f8faa9d0dc169e01
Adam Quinton <quintonish@gmail.com>
2016-10-15 10:05:17 -0600

    Add download fullsize feature to lightbox

--- lightbox.js
+++ lightbox.js
@@ -1,8 +1,9 @@
 function getAndShow(webUrl, thumbUrl, fullsizeUrl, album) {
+  var downloadUrl = thumbUrl.substr(thumbUrl.lastIndexOf("/")+1);
   document.getElementById('lightbox-picture').setAttribute('src', webUrl);
-  document.getElementById('fullsize-link').setAttribute('href', fullsizeUrl);
-  var thumbName = thumbUrl.substr(thumbUrl.lastIndexOf("/")+1);
-  document.getElementById('set-as-album-thumb').setAttribute('href', `set_album_thumb.php?album=${album}&thumb=${thumbName}`);
+  document.getElementById('download-link').setAttribute('href', fullsizeUrl);
+  document.getElementById('download-link').setAttribute('download', downloadUrl);
+  document.getElementById('set-as-album-thumb').setAttribute('href', `set_album_thumb.php?album=${album}&thumb=${downloadUrl}`);
   document.getElementById('light').style.display = 'block';
   document.getElementById('fade').style.display = 'block';
 }
--- pics.php
+++ pics.php
@@ -24,7 +24,7 @@ if ($handle = opendir('../photo')) {
   <img id="lightbox-picture"/>
   <div id="lightbox-sidebar">
     <a href="javascript:void(0)" onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</a>
-    <br/><a id="fullsize-link">Full-size</a>
+    <br/><a id="download-link">Download</a>
     <br/><a id="set-as-album-thumb">Set as Album Thumb</a>
   </div>
 </div>
