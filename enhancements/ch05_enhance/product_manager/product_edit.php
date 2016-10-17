35984844fc87c289a272495c68935f59469cbc2b
Adam Quinton <quintonish@gmail.com>
2016-10-17 15:03:00 -0600

    5 enhancement stub

---
+++ enhancements/ch05_enhance/product_manager/product_edit.php
@@ -0,0 +1,49 @@
+<?php include '../view/header.php'; ?>
+<main>
+    <h1>Edit Product</h1>
+    <form action="index.php" method="post" id="add_product_form">
+        <input type="hidden" name="action" value="add_product">
+
+        <label>Category:</label>
+        <select name="category_id">
+        <?php
+            $code = $product['productCode'];
+            $name = $product['productName'];
+            $listPrice = $product['listPrice'];
+        ?>
+        <?php foreach ( $categories as $category ) : ?>
+          <?php
+            $selected = "";
+            if ($category_id == $category['category_id']) {
+              $selected = "selected='selected'";
+            }
+          ?>
+            <option <?php echo $selected; ?>value="<?php echo $category['categoryID']; ?>">
+                <?php echo $category['categoryName']; ?>
+            </option>
+        <?php endforeach; ?>
+        </select>
+        <br>
+
+        <label>Code:</label>
+        <input type="text" name="code" <?php echo "value='$code'"; ?> />
+        <br>
+
+        <label>Name:</label>
+        <input type="text" name="name" <?php echo "value='$name'"; ?> />
+        <br>
+
+        <label>List Price:</label>
+        <input type="text" name="price" <?php echo "value='$listPrice'"; ?>/>
+        <br>
+
+        <label>&nbsp;</label>
+        <input type="submit" value="Add Product" />
+        <br>
+    </form>
+    <p class="last_paragraph">
+        <a href="index.php?action=list_products">View Product List</a>
+    </p>
+
+</main>
+<?php include '../view/footer.php'; ?>
